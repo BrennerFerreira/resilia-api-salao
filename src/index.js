fad6a23dae1fd7d9d8cfa8e15e930e3528351f98
BrennerFerreira <brennercsferreira@gmail.com>
2021-11-22 19:33:59 -0300

    feat: add dotenv safe

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ const express = require("express");
 const appRouter = require("./router/appRouter");
 const configServer = require("./server/config");
 
+const { PORT } = process.env;
+
 const app = express();
 
 configServer(app);
 appRouter(app);
 
-app.listen(process.env.PORT, (error) => {
+app.listen(PORT, (error) => {
   if (error) {
     console.log(`Error: ${error}`);
   }
-  console.log(`Server running on port: ${process.env.PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
