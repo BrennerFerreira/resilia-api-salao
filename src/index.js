99c1ec24290a5e0d37bd1aeb8c7cb1e5686c929c
BrennerFerreira <brennercsferreira@gmail.com>
2021-11-22 19:12:57 -0300

    fix: corrigir procfile

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ const express = require("express");
 const appRouter = require("./router/appRouter");
 const configServer = require("./server/config");
 
-const { PORT } = process.env;
-
 const app = express();
 
 configServer(app);
 appRouter(app);
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.listen(process.env.PORT, (error) => {
+  if (error) {
+    console.log(`Error: ${error}`);
+  }
+  console.log(`Server running on port: ${process.env.PORT}`);
+});
