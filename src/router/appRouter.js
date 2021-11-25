ae44f197e92211e9519994e2f68e2060a586a0cb
BrennerFerreira <brennercsferreira@gmail.com>
2021-11-25 18:49:29 -0300

    feat: adiciona cors

--- src/router/appRouter.js
+++ src/router/appRouter.js
@@ -1,10 +1,12 @@
 const { Router, json } = require("express");
+const cors = require("cors");
 
 const usersRouter = require("./usersRouter");
 
 const router = Router();
 
 router.use(json());
+router.use(cors());
 
 router.use("/users", usersRouter);
 
