4d3cdc18ffe8a9e725821ce830067509ea7634b7
Vitória Bernardino <vitorianbernardino@gmail.com>
2021-11-28 10:11:07 -0300

    feat: cria controller agendamentos

---
+++ src/controllers/scheduleController.js
@@ -0,0 +1,22 @@
+const db = require('../db/scheduleDb')
+const model = require('../models/ScheduleWithoutId')
+class ScheduleController {
+    constructor(db){
+        this.db = db
+    }
+    createSchedule = async (req, res) => {
+        const {userId} = req.body
+        let data = new Date()
+        data.setDate(data.getDate() + 7)
+        const schedule = new model(userId, data)
+        const create = await db.createSchedule(schedule)
+            console.log(create)
+            if(create){
+                res.status(201).send(create)
+            }
+            else{
+                res.status(500).send("Deu Erro")
+            }
+    }
+}
+module.exports = new ScheduleController(db)
