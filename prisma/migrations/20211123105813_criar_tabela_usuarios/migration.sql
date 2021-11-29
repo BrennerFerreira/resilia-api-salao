2f094118ef757b2394232697cf6b1261048faeb3
Beatriz Medeiros <biamedcost@hotmail.com>
2021-11-29 18:45:53 -0300

    feat correção da tabela services

--- prisma/migrations/20211123105813_criar_tabela_usuarios/migration.sql
+++ prisma/migrations/20211123105813_criar_tabela_usuarios/migration.sql
@@ -1,7 +1,13 @@
 -- CreateTable
-CREATE TABLE "Services" (
-    "employeeId" TEXT NOT NULL,
-    "serviceId" TEXT NOT NULL,
-    "price" DECIMAL NOT NULL,
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
-    CONSTRAINT "Services_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
