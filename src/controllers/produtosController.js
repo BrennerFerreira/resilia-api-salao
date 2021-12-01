09340dd4a7106e5d4b44cef25788466657b27ab8
João Paulo <joaopaulogomesdasilva99@gmail.com>
2021-12-01 12:21:16 -0300

    creating product routes and controls

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -8,8 +8,8 @@ class ProdutosController {
       }
       createProduct = async (req, res) => {
           const {name, preco} = req.body;
-      
           const produto = new ProdutosSemID(name, preco);
+
           const produtoCriado = await this.db.createProdutos(produto);
           if (produtoCriado) {
               res.status(201).send(produtoCriado)
@@ -17,6 +17,72 @@ class ProdutosController {
               res.status(500).send("Produto não cumpre os criterios minimos");
           }
         }
+
+
+findProduct = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+      res.status(400).send({
+        error: "Missing required argument (must have id)",
+      });
+
+      return;
+    }
+
+    const productFromDb = await this.db.findProduct(id);
+
+    if (productFromDb) {
+      const product = new ProductGet(
+        productFromDb.id,
+        productFromDb.name,
+        productFromDb.preco
+      );
+      res.send(product);
+    } else {
+      res.status(404).send();
+    }
+  };
+
+  findAll = async (req, res) => {
+    const productFromDb = await this.db.findAll();
+
+    if (productFromDb) {
+      const product = productFromDb.map(
+        (product) => new UserGet(product.id, product.name, product.preco)
+      );
+      res.send(product);
+    } else {
+      res.status(500).send({ error: "Error while trying to retrieve product" });
+    }
+  };
+
+  update = async (req, res) => {
+    const { id } = req.params;
+    const { name, preco } = req.body;
+    const product = new ProductCreate(name, preco);
+
+    const updatedProduct = await productsDb.updateProduct(id, product);
+
+    if (updatedProduct) {
+      res.status(204).send();
+    } else {
+      res.status(500).send({ error: "Error while trying to retrieve products" });
+    }
+  };
+
+  remove = async (req, res) => {
+    const { id } = req.params;
+
+    const removedId = await ProductsDb.removeProduct(id);
+
+    if (removedId) {
+      res.status(204).send();
+    } else {
+      res.status(500).send({ error: "Error while trying to retrieve Products" });
+    }
+  };
 }
+
 
 module.exports = new ProdutosController(ProdutosDb);
--- src/router/produtosRouter.js
+++ src/router/produtosRouter.js
@@ -4,9 +4,9 @@ const { Router } = require("express");
 const router = Router();
 
 router.post("/", ProdutosController.createProduct);
-// router.get("/:id", ProdutosController.findUser);
-// router.get("/", ProdutosController.findAll);
-// router.patch("/:id", ProdutosController.update);
-// router.delete("/:id", ProdutosController.remove);
+router.get("/:id", ProdutosController.findProduct);
+router.get("/", ProdutosController.findAll);
+router.patch("/:id", ProdutosController.update);
+router.delete("/:id", ProdutosController.remove);
 
 module.exports = router;
